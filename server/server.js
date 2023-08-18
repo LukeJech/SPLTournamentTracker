a96c46897f9d27859563f9f8a4603ac29f32dffb
LukeJech <LukeJech@gmail.com>
2023-08-17 17:10:40 -0700

    Player routing created

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 
 const express = require('express');
+const playerRoutes =  require('./routes/players.ts')
 
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // routes
-app.get('/', (req, res) => { 
-    res.json({ message: "Welcome to my application." });
-});
+app.use('/api/players', playerRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {

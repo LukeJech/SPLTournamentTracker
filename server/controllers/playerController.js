3099a36ee1e3314f76bb4ef300b0d738c20756c1
LukeJech <LukeJech@gmail.com>
2023-08-22 20:09:11 -0700

    player update functionality

--- server/controllers/playerController.js
+++ server/controllers/playerController.js
@@ -1,14 +1,14 @@
 const Player = require('../models/Player.ts');
 const mongoose = require('mongoose');
 
-// get all workous
+// get all player
 const getAllPlayers = async (req, res) => {
         const players = await Player.find({}).sort({name:1});
         res.status(200).json(players);
 
 }
 
-// get a single workout
+// get a single player by id
 const getPlayer = async (req, res) => {
     const {id} = req.params;
 
@@ -25,11 +25,20 @@ const getPlayer = async (req, res) => {
     res.status(200).json(player);
 }
 
+// get a single player by name
+const getPlayerByName = async (name) => {
+    try {
+        const player = await Player.findOne({ name })
+        return player
+    } catch(error){
+        throw new Error(error.message)
+    }
+}
+
 // create a new player
 const createPlayer = async (name, points, placements) => {
 
     // add doc to db
-    console.log(name, points, placements)
     try{
         const player = await Player.create({name, points, placements});
         return player;
@@ -57,29 +66,40 @@ const deletePlayer = async (req, res) => {
 }
 
 // update a player
-const updatePlayer = async (req, res) => {
-    const {id} = req.params;
-    const {name, points} = req.body;
+const updatePlayer = async (name, points, placements) => {
+    try{
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({err: 'Player not found'});
+        const updatedPlayer = await Player.findOneAndUpdate(
+            { name },
+            {
+                $inc: {
+                    'points.bronzeModern': points.bronzeModern,
+                    'points.bronzeWild': points.bronzeWild,
+                    'points.silverModern': points.silverModern,
+                    'points.silverWild': points.silverWild,
+                    'points.goldModern': points.goldModern,
+                    'points.goldWild': points.goldWild,
+                    'points.diamondModern': points.diamondModern,
+                    'points.diamondWild': points.diamondWild,
+                    'placements.first': placements.first,
+                    'placements.second': placements.second,
+                    'placements.third': placements.third,
+                    'placements.top10': placements.top10,
+                }
+            },
+            {new: true}
+            );
+            return updatedPlayer
+    } catch(err) {
+        throw new Error(err.message)
     }
 
-    const player = await Player.findOneAndUpdate({_id: id}, 
-        {
-            ...req.body,
-        });
-
-        if (!player) {
-            return res.status(404).json({err: 'Player not found'});
-        }
-    
-        res.status(200).json(player);
 }
 
 module.exports = {
     createPlayer,
     getAllPlayers,
+    getPlayerByName,
     getPlayer,
     deletePlayer,
     updatePlayer

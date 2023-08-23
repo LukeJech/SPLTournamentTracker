10e84afde42cd7b5754f71955e1ff5f32140fb4e
LukeJech <LukeJech@gmail.com>
2023-08-22 17:27:23 -0700

    player podium placement calculations by tournament type

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -4,7 +4,7 @@ const { calculate_points } = require('../calculations/score_calculations');
 
 const getTournament = async () => {
   try {
-    const response = await fetch('https://api.splinterlands.com/tournaments/find?id=ac856fadd2c3c1c1156be158c069b482a6a563f8');
+    const response = await fetch('https://api.splinterlands.com/tournaments/find?id=c23d4d319977cfe04dce448fba6bf372c3072a4e');
 
     if (!response.ok) {
       console.error('API network response was not ok:', response.status);
@@ -16,22 +16,25 @@ const getTournament = async () => {
         
         const playerName = data.players[0].player
         const points = calculate_points(data.players[0].finish)
-        const createdPlayer = await PlayerController.createPlayer(playerName, {
-                bronzeModern: 0,
-                bronzeWild: 0, 
-                silverModern: points,
-                silverWild: 0,
-                goldModern: 0,
-                goldWild: 0,
-                diamondModern: 0,
-                diamondWild: 0,
-        },
-        {
+        const player_points = {
+                bronzeModern: data.data.rating_level === 1 && data.data.allowed_cards.epoch === 'modern' ? points: 0,
+                bronzeWild: data.data.rating_level === 1 && data.data.allowed_cards.epoch === 'wild' ? points: 0,
+                silverModern: data.data.rating_level === 2 && data.data.allowed_cards.epoch === 'modern' ? points: 0,
+                silverWild: data.data.rating_level === 2 && data.data.allowed_cards.epoch === 'wild' ? points: 0,
+                goldModern: data.data.rating_level === 3 && data.data.allowed_cards.epoch === 'modern' ? points: 0,
+                goldWild: data.data.rating_level === 3 && data.data.allowed_cards.epoch === 'wild' ? points: 0,
+                diamondModern: data.data.rating_level === 4 && data.data.allowed_cards.epoch === 'modern' ? points: 0,
+                diamondWild: data.data.rating_level === 4 && data.data.allowed_cards.epoch === 'wild' ? points: 0,
+        }
+        console.log(points, player_points, 'ratinglevel', data.data.rating_level)
+        player_placement = {
           first: data.players[0].finish === 1 ? 1 : 0,
           second: data.players[0].finish === 2 ? 1 : 0,
           third: data.players[0].finish === 3 ? 1 : 0,
           top10: data.players[0].finish >= 4 && data.players[0].finish <= 10 ? 1 : 0,
-        });
+        }
+
+        const createdPlayer = await PlayerController.createPlayer(playerName, player_points, player_placement);
     }
     return data;
 
